b67cf7d9db5dcca0a2ec7329164765d2ec7b9d94
dx2331lxz <3434055686@qq.com>
2023-12-07 20:02:30 +0800

    demo

---
+++ itglm/vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // 处理跨域
+  server: {
+    proxy: {
+      '/document': {
+        target: 'http://chat.qdjzxny.com',
+        changeOrigin: true
+      }
+    },
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
